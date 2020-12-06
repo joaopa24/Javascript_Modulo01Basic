68525249cd132dab5ccc6aaf9471030d1760459a
joknz21 <joaokn129@gmail.com>
2020-12-06 20:56:22 -0300

    add impar e par

---
+++ Number.js
@@ -0,0 +1,11 @@
+/* return odd or even number */
+
+function verif(number){
+    if(number % 2 === 0){
+      return console.log(`${number} is even`)
+    }
+    else{
+      return console.log(`${number} is odd`)
+    }
+}
+verif(4)
